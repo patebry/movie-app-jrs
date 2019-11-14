36bbf666e3a75e7f2bdb75861bb00f5c53559f4e
Pate Bryant <patebry@gmail.com>
2019-11-14 11:15:20 -0500

    added input for title

--- src/App.js
+++ src/App.js
@@ -5,10 +5,40 @@ import './App.css'
 
 function App() {
   const [movies, setMovies] = useState(defaultMovies)
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    id: Math.random(),
+    rank: null
+  })
   console.log(movies)
   return (
     <div className='App'>
       <Header />
+      <div>{movies.length}</div>
+      <input
+        type='text'
+        name='title'
+        value={newMovie.title}
+        placeholder='title'
+        onChange={e => {
+          e.persist()
+          setNewMovie({
+            ...newMovie,
+            title: e.target.value
+          })
+        }}
+      />
+      <div className='movieWrapper'>
+        {movies &&
+          movies.length > 0 &&
+          movies.map((x, i) => {
+            return (
+              <div key={i} className='movieCard'>
+                {x.title}
+              </div>
+            )
+          })}
+      </div>
     </div>
   )
 }
