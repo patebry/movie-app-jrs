3acca8ff22515be9151ccbfcfc3f666687e0a102
Pate Bryant <patebry@gmail.com>
2019-11-14 11:00:48 -0500

    adding movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/header'
+import defaultMovies from './defaultMovies.json'
 import './App.css'
 
 function App() {
+  const [movies, setMovies] = useState(defaultMovies)
+  console.log(movies)
   return (
     <div className='App'>
       <Header />
